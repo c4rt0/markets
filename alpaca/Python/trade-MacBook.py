5308d89cbe848908b7efae69a17d86abfaf833de
Adam Piasecki <c4rt0gr4ph3r@gmail.com>
2021-11-02 15:48:01 +0000

    Playing arround with Alpaca

--- alpaca/Python/trade-MacBook.py
+++
@@ -1,7 +0,0 @@
-def isiterable(object):
-    try:
-        iter(object)
-        
-        return [True, print(object), print( "is iterable")]
-    except TypeError: #not iterable
-        return False
