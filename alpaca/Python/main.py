1a07fa5750f692e0ceb1900ef93a37b3d9b9c8de
Adam Piasecki <c4rt0gr4ph3r@gmail.com>
2021-11-04 20:47:18 +0000

    Workinf on FastAPI

--- alpaca/Python/main.py
+++ alpaca/Python/main.py
@@ -3,12 +3,15 @@ import graphene
 from fastapi import FastAPI
 from starlette.graphql import GraphQLApp
 
+
 class Query(graphene.ObjectType):
     hello = graphene.String(name=graphene.String(default_value=", World!"))
-    def resolve_hello(self,info,name):
+
+    def resolve_hello(self, info, name):
         return "Hello " + name
+
 
 app = FastAPI()
 
 # Managed to teg it up and running, using : https://www.youtube.com/watch?v=2_puWfTK8bQ&t=604s
-app.add_route("/", GraphQLApp(schema=graphene.Schema(query=Query)))
+app.add_route("/graphql", GraphQLApp(schema=graphene.Schema(query=Query)))
